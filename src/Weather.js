f52d7e67f5da65d691adafc6639b110e4e9cd287
michalina-galczak <70964363+michalina-galczak@users.noreply.github.com>
2021-02-07 21:26:51 +0000

    Added material icons. Change buttons style

--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,8 @@ import "./Weather.css";
 import Location from "./Location.js";
 import Info from "./Info.js";
 import Forecast from "./Forecast.js";
+import SearchIcon from '@material-ui/icons/Search';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 
 export default function Weather() {
     return (
@@ -10,14 +12,18 @@ export default function Weather() {
             <div className="row">
                 <form>
                     <div className="row">
-                        <div className="col-8">
+                        <div className="col-10 Weather-search">
                             <input type="Search" placeholder="Enter a city..." className="form-control" />
                         </div>
-                        <div className="col-2">
-                            <input type="Submit" className="btn Weather-btn-search" value="Search" />
+                        <div className="col-1">
+                            <button type="Submit" className="btn Weather-btn-search" value="Search">
+                                <SearchIcon />
+                            </button>    
                         </div>
-                        <div className="col-2">
-                            <input type="Submit" className="btn Weather-btn-location" value="Location" />
+                        <div className="col-1">
+                            <button type="Submit" className="btn Weather-btn-location" value="Location">
+                                <LocationOnIcon />
+                            </button>    
                         </div>
                     </div>
                     <div className="row Weather-location">
